e91b9a9c84a2bbc69a57a7350de9e19d2bb7d3ca
PCarlin <pcarlin2014@gmail.com>
2019-10-28 14:50:51 -0400

    benchmark commit final

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,6 +1,11 @@
 #benchmark
 
-install.packages('microbenchmark')
-require(microbenchmark)
+#install.packages('microbenchmark')
 
-cat(microbenchmark(llr,compute_f_hat))
+library.path = c("C:/Users/Pat C/Documents/R/win-library/3.5", "C:/Program Files/R/R-3.5.2/library")
+
+library('microbenchmark', lib.loc = library.path)
+
+source('llr_functions.R')
+
+microbenchmark(llr(x, y, z, omega=1))
