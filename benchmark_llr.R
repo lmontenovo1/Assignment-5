b4a1013bc321d8b13e3091f0e1a68dce0d844f0c
PCarlin <pcarlin2014@gmail.com>
2019-10-28 13:16:13 -0400

    first change supplement_2

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,6 +1,11 @@
 #benchmark
 
 #install.packages('microbenchmark')
+if (!require("microbenchmark")) {
+  install.packages("microbenchmark", repos="http://cran.rstudio.com/") 
+  library("microbenchmark")
+}
+
 require(microbenchmark)
 
 microbenchmark(llr(x, y, z, omega))
